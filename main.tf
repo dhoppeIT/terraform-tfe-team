74ea219b3eb51733eb0185f5db8b40912ebb63a3
Dennis Hoppe <github@dhoppe.it>
2021-12-03 17:16:59 +0100

    fix: Use data source to get the ID of the team * The free plan of Terraform Cloud/Enterprise   does not support the management of teams

--- main.tf
+++ main.tf
@@ -1,17 +1,16 @@
-resource "tfe_team" "default" {
+data "tfe_team" "default" {
   name         = var.name
   organization = var.organization
-  visibility   = var.visibility
 }
 
 resource "tfe_team_organization_member" "default" {
   count = length(var.organization_membership_id)
 
-  team_id                    = tfe_team.default.id
+  team_id                    = data.tfe_team.default.id
   organization_membership_id = count.index
 }
 
 resource "tfe_team_token" "default" {
-  team_id          = tfe_team.default.id
+  team_id          = data.tfe_team.default.id
   force_regenerate = var.force_regenerate
 }
