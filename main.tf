36784e5833c5908e31af6ea383bb370a9c258adf
Dennis Hoppe <github@dhoppe.it>
2021-12-03 17:26:14 +0100

    fix: Add variable to count.index * Otherwise the organization_membership_id argument   contains a numberic value

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "tfe_team_organization_member" "default" {
   count = length(var.organization_membership_id)
 
   team_id                    = data.tfe_team.default.id
-  organization_membership_id = count.index
+  organization_membership_id = var.organization_membership_id[count.index]
 }
 
 resource "tfe_team_token" "default" {
