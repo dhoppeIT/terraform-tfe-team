0bdbcbf3764654745eedcbc4c62b7ffaf8a4295f
Dennis Hoppe <github@dhoppe.it>
2022-02-02 20:37:00 +0100

    feat: Add ID of the token as output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
+output "id" {
+  value       = tfe_team_token.default.id
+  description = "The ID of the token"
+}
+
 output "token" {
   value       = tfe_team_token.default.token
   description = "The generated token"
